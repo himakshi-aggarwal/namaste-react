064a3f3ad11c84cbad05f528a0ca9f31a321fc73
Himakshi Aggarwal <hiag@softwareag.com>
2024-05-06 17:33:01 +0530

    second episode notes

--- ep2/app.js
+++ ep2/app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom"
 
 const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm H1"),React.createElement("h2", {}, "I'm H2")]))
 const root = ReactDOM.createRoot(document.getElementById('root'))
